95c420f42066f8e9f32c6f2da9b51b710b653d30
maximethibault <maxime.thibault@ia.ca>
2016-11-08 13:29:33 -0500

    Add files via upload

---
+++ runbookiA/LocalAdmin.ps1
@@ -0,0 +1,13 @@
+﻿$Admin = "Azure"
+#get users in local administrator
+$obj_group = [ADSI]"WinNT://$($env:COMPUTERNAME)/Administrators,group"
+$Administrators = @($obj_group.psbase.Invoke("Members")) | foreach{([ADSI]$_).InvokeGet("Name")}
+If ($Admin -eq $Administrators)
+{ 
+    Write-Output "Administrateur local" $Administrators
+    $Administrators | Out-File C:\filename.txt
+} 
+else
+{ 
+    Write-Output "Non Conforme"
+}
